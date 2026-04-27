fb25dda03f5a4687366132318d10639c42a197d1
crrow <41375382+crrow@users.noreply.github.com>
2026-04-27 09:13:32 +0000

    chore(release): bump version to 0.3.0

--- version.go
+++ version.go
@@ -4,4 +4,4 @@ package main
 // Version is the semantic version of devkit.
 // For development builds, this will be "dev".
 // For release builds, run: just version-update
-const Version = "0.2.0"
+const Version = "0.3.0"
